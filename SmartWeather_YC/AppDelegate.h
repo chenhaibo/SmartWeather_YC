243eb871e6350386dfb54b610768ec815244875f
ChenHaiBo <179572179@qq.com>
2013-11-25 18:16:00 +0800

    Initial Commit

---
+++ SmartWeather_YC/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  SmartWeather_YC
+//
+//  Created by haibo chen on 13-11-25.
+//  Copyright (c) 2013年 haibo chen. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ SmartWeather_YC/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  SmartWeather_YC
+//
+//  Created by haibo chen on 13-11-25.
+//  Copyright (c) 2013年 haibo chen. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
